8ef33702d23b7cd8e4fbfdf05fb282a6a508be29
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-03-06 04:24:01 -0300

    formulario que retorna os valores

--- teste/web/main.dart
+++ teste/web/main.dart
@@ -1,5 +1,20 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output')?.text = 'Olá, Dart na Web!';
+  // Selecionar o botão de envio
+  ButtonElement submitButton = querySelector('#submitBtn') as ButtonElement;
+  
+  // Adicionar evento de clique ao botão
+  submitButton.onClick.listen((event) {
+    // Obter os valores dos campos de input
+    String name = (querySelector('#name') as InputElement).value ?? '';
+    String age = (querySelector('#age') as InputElement).value ?? '';
+
+    // Exibir a saudação personalizada
+    if (name.isNotEmpty && age.isNotEmpty) {
+      querySelector('#greeting')?.text = 'Olá, $name! Você tem $age anos.';
+    } else {
+      querySelector('#greeting')?.text = 'Por favor, preencha todos os campos.';
+    }
+  });
 }
