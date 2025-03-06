9db8298bccbc5cf1bc6f498f4a3fcdc14245699e
Felipe Monari <133614580+Monari14@users.noreply.github.com>
2025-03-06 04:35:29 -0300

    pegando dado de api

--- teste/web/main.dart
+++ teste/web/main.dart
@@ -1,20 +1,17 @@
 import 'dart:html';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
-  // Selecionar o botão de envio
-  ButtonElement submitButton = querySelector('#submitBtn') as ButtonElement;
-  
-  // Adicionar evento de clique ao botão
-  submitButton.onClick.listen((event) {
-    // Obter os valores dos campos de input
-    String name = (querySelector('#name') as InputElement).value ?? '';
-    String age = (querySelector('#age') as InputElement).value ?? '';
+void main() async {
+  // Faz a requisição para a API
+  var response = await http.get(Uri.parse('https://pokeapi.co/api/v2/pokemon/ditto'));
 
-    // Exibir a saudação personalizada
-    if (name.isNotEmpty && age.isNotEmpty) {
-      querySelector('#greeting')?.text = 'Olá, $name! Você tem $age anos.';
-    } else {
-      querySelector('#greeting')?.text = 'Por favor, preencha todos os campos.';
-    }
-  });
+  if (response.statusCode == 200) {
+    // Converte a resposta em um Map
+    var data = jsonDecode(response.body);
+    //printa no h1 que o id é output
+    querySelector('#output')!.text = 'Pegando valor do json da api: ${data['name']}'; // o name é como ta no json
+  } else {
+    querySelector('#output')!.text = 'Erro ao carregar dados';
+  }
 }
